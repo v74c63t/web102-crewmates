69a17dde3b5e452ad4d457b807a5c1a80d61f1a1
Vanessa Tang <vanesst5@uci.edu>
2024-04-12 01:54:05 -0700

    Minor adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <div className='home'>
-        <h2 className='welcome'>Welcome to the Character Creator!</h2>
+        <h2 className='welcome'>Welcome to the Honkai: Star Rail Character Creator!</h2>
         <h4 className='description'>Here you can create your very own set of characters for trailblazing!</h4>
         <img src="https://upload-os-bbs.hoyolab.com/upload/2022/05/30/196282833/748e09dee0d41a97ef663bed24f6b551_7455891618893644475.jpg" alt="March 7th" width={500} height={'auto'} />
       </div>
