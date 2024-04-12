604477f059fae9efe174c41e2829afeb78afb0f1
Vanessa Tang <vanesst5@uci.edu>
2024-04-11 19:34:39 -0700

    Added basic gallery page

--- src/pages/ReadCrew/ReadCrew.jsx
+++ src/pages/ReadCrew/ReadCrew.jsx
@@ -1,7 +1,57 @@
+import { useState, useEffect } from "react"
+import { supabase } from "../../Client"
+import './ReadCrew.css'
+import { Link } from "react-router-dom"
 
 const ReadCrew = () => {
+  const [characters, setCharacters] = useState([])
+  const [message, setMessage] = useState("Loading...")
+
+  useEffect(() => {
+    const fetchCharacters = async() => {
+      const {data} = await supabase
+                          .from('crewmates')
+                          .select()
+                          .order('created_at', {ascending: false})
+      setCharacters(data)
+      if(data.length === 0) {
+        setMessage("No Characters Yet")
+      }
+    }
+    fetchCharacters()
+  }, [])
+
+
   return (
-    <div>Read</div>
+    <div>
+      <div className="view">
+      <h2>Character Gallery</h2>
+        {
+          characters !== null && characters.length > 0 ? (
+            <div>
+              {
+                characters.map((character, i) => {
+                  return (
+                    <div key={i}>
+                      <div>{character.name}</div>
+                      <div>{character.path}</div>
+                      <div>{character.element}</div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          ) : <h4>{message}</h4>
+        }
+        {message === 'No Characters Yet' ? (
+          <h4>
+            <Link to="/create">
+              Start by Creating a Character Here
+            </Link>
+          </h4>
+        ): ""}
+      </div>
+    </div>
   )
 }
 
